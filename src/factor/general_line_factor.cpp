25190a93b9e228f9f1e151c6e606c41fed79a50a
xubo <2816955252@qq.com>
2021-03-18 23:29:40 +0800

    init

--- src/factor/general_line_factor.cpp
+++
@@ -1,7 +0,0 @@
-//
-// Created by ubuntu on 2021/3/6.
-//
-#include "general_line_factor.h"
-
-Eigen::Matrix2d general_line::GeneralLineErrorOneCameraTerm::sqrtInfo_;
-Eigen::Matrix2d general_line::GeneralLineErrorTwoCameraTerm::sqrtInfo_;
